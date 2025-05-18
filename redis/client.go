1cfabe46a93114b751ab8f120057d45e3bb41d28
ketul-andev <ketul@adnabu.com>
2025-05-19 00:13:47 +0530

    Updated Redis client configuration to use environment variable for production settings, defaulting to localhost for development.

--- redis/client.go
+++ redis/client.go
@@ -1,12 +1,20 @@
 package redisconn
 
 import (
+	"os"
+
 	"github.com/redis/go-redis/v9"
 )
 
 func NewRedisClient() *redis.Client {
+	var redisURL string
+	if os.Getenv("ENV") == "production" {
+		redisURL = os.Getenv("REDIS_URL")
+	} else {
+		redisURL = "localhost:6379"
+	}
 	return redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr: redisURL,
 		DB:   0,
 	})
 }
